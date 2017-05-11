4ec12327263b1bfb314ed38d2469ec9ebc33ee9d
Leandro Peñafiel <leopm87@gmail.com>
2017-05-10 19:45:20 -0500

    rounding-numbers

---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+var roundUp= 1.5;//declaramos una variable
+var rounded= Math.round(roundUp);//esta funcion sirve para redondear el numero decimal a un entero
+console.log(rounded); //imprime la funcion
